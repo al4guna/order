b3289f8b1a7f36ec60f9f5cf66725bb816f76f5d
Angel Laguna Garrido <al4guna@gmail.com>
2020-07-19 00:21:10 -0500

    creating the order model

---
+++ src/model/order/index.js
@@ -0,0 +1,47 @@
+const mongoose =  require('mongoose')
+const { Schema , model } = mongoose
+
+const orderSchema = Schema({
+	chanel: {
+		type: String,
+		required: true
+	}, 
+	state: {  
+		type: String,
+		enum: ['Reservada', 'Pendiente', 'En tránsito', 'Lista para recoger', 'Cerrada', 'Cancelada'],
+		required: true
+	},
+	value: { 
+		type: Number,
+		required: true
+	},
+	discount: { 
+		type: Number,
+		required: true
+	},
+	date:  { 
+		type: String 
+	},
+	deliveryType: { 
+		type: String,
+		enum: ['Estandar', 'Express'],
+		required: true
+	},
+	shippingType: {
+		type: String,
+		enum: ['Entrega en tienda', 'Entrega en domicilio'],
+		required: true
+	},
+	items: {
+		type: [{
+			sky: String, 
+			nombre: String, 
+			cantiad: Number,
+			precio: Number, 
+			codigoBarra: String
+		}],
+		default: []
+	}
+})
+
+module.exports = model('Order', orderSchema)
