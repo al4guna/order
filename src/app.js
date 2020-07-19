2de558aa98057d6bbcde99a12a8943f3e43332b7
Angel Laguna Garrido <al4guna@gmail.com>
2020-07-19 01:45:34 -0500

    adding error middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const route = require('./route')
+const { errorMiddleware } = require('./middlewares/error.js')
+
 
 const app = express();
 
 app.use(express.json())
 
 app.use('/api', route)
+
+app.use(errorMiddleware);
 
 module.exports = app
