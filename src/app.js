acc7087af392245387f76dd279a244dd390cf5b1
Angel Laguna Garrido <al4guna@gmail.com>
2020-07-20 15:40:57 -0500

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const route = require('./route')
+const cors = require('cors')
 const { errorMiddleware } = require('./middlewares/error.js')
 
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
-
 app.use('/api', route)
 
 app.use(errorMiddleware);
