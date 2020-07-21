b8cb1f8c6a54298895ae7c43f00b50e5f3a0aecc
Angel Laguna Garrido <al4guna@gmail.com>
2020-07-21 18:54:12 -0500

    creating parserOrderUpdate

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,3 +1,5 @@
+const product = require("../model/product");
+
 const parseOrder = (order) => {
 	if(order) {
 		return {
@@ -13,5 +15,40 @@ const parseOrder = (order) => {
 	}
 	return null;
 }
+const parseOrderUpdate = (order) => {
+	if (!order) {
+		return null
+	}
+	if(order.value == undefined) {
+		return { id: order.id, state : order.state }
+	}
+	if(order.state === undefined ) {
+		return { id: order.id, value : order.value }
+	}
+	return {
+		id: order.id,
+		value: order.value,
+		state: order.state
+	}
+}
 
-module.exports = { parseOrder }
+const parseProduct = (product) => {
+	if(product) {
+		return {
+			id: product._id,
+			sku: product.sku,
+			name: product.name,
+			quantity: product.quantity,
+			price: product.price,
+			barcode: product.barcode,
+			idOrder: product.idOrder
+		}
+	}
+	return null;
+}
+
+module.exports = { 
+	parseOrder,
+	parseOrderUpdate,
+	parseProduct
+}
