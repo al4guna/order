ab5facf28642ae35dc424171fd063cd878f9f0c9
Angel Laguna Garrido <al4guna@gmail.com>
2020-07-21 19:05:50 -0500

    modifying service order

--- src/service/order/index.js
+++ src/service/order/index.js
@@ -21,5 +21,20 @@ const GetAll = () => new Promise((resolve, reject) => {
 			reject({ statusCode: 500, message: reason.message, keyError: errorList.internalDB })
 		})
 })
+
+const Update = (dat) => new Promise((resolve, reject) => {
+	Order.findOneAndUpdate({_id: dat.id }, dat, {
+		new: true,
+		upsert: true
+	})
+		.then((data) => {
+			resolve(data)
+		})
+		.catch(() => {
+			reject({ statusCode: 500, message: reason.message, keyError: errorList.internalDB })
+		})
+})
+
 exports.Save = Save
 exports.GetAll = GetAll
+exports.Update = Update
